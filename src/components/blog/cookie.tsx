b9c655b917d535621af5da3198db7a45aa26d02e
Charlie Wu <charliewu500@gmail.com>
2025-06-26 19:07:32 +0800

    [change] 調整 cookie 設定

--- src/components/blog/cookie.tsx
+++ src/components/blog/cookie.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 async function setCookie(cookieName: string) {
     const res = await fetch('https://backendnestjs-production-aeb2.up.railway.app/cookie/setCookie', {
@@ -9,25 +9,29 @@ async function setCookie(cookieName: string) {
         body: JSON.stringify({ value: cookieName }),
     });
     const data = await res.json();
-    return data;
+    console.log('setCookie', data);
+    return data.message;
 }
 
 async function seeCookie() {
     const res = await fetch('https://backendnestjs-production-aeb2.up.railway.app/cookie/seeCookie', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        method: 'GET',
+        credentials: 'include',
     });
     const data = await res.json();
-    return data;
+    console.log('seeCookie', data);
+    return data.message;
 }
 
 async function dropCookie() {
     const res = await fetch('https://backendnestjs-production-aeb2.up.railway.app/cookie/dropCookie', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
     });
     const data = await res.json();
-    return data;
+    console.log('dropCookie', data);
+    return data.message;
 }
 
 export default function ZustandTest() {
@@ -36,26 +40,24 @@ export default function ZustandTest() {
     const [seeReturn, setSeeReturn] = useState<string>('按這裡查看 cookie');
     const [dropReturn, setDropReturn] = useState<string>('按這裡刪除 cookie');
 
-    useEffect(() => {}, []);
-
     return (
         <div className="flex flex-col items-center justify-center bg-gray-100 p-4 gap-4">
             <input type="text" placeholder={myCookie} onChange={(e) => setMyCookie(e.target.value)} />
             <div className="flex items-center gap-2">
                 <span>{setReturn}</span>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setSetReturn(JSON.stringify(setCookie(myCookie)))}>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={async () => setSetReturn(await setCookie(myCookie))}>
                     送出
                 </button>
             </div>
             <div className="flex items-center gap-2">
                 <span>{seeReturn}</span>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setSeeReturn(JSON.stringify(seeCookie()))}>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={async () => setSeeReturn(await seeCookie())}>
                     送出
                 </button>
             </div>
             <div className="flex items-center gap-2">
                 <span>{dropReturn}</span>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setDropReturn(JSON.stringify(dropCookie()))}>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={async () => setDropReturn(await dropCookie())}>
                     送出
                 </button>
             </div>
